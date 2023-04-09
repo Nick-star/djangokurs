fb0b32173dd2235f1228acdaa96688e7a473372b
Nick-star <stavnikita@gmail.com>
2023-04-09 17:56:22 +0700

    Models and tables are created

---
+++ baskets/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+from products.models import Product
+
+
+class Basket(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} - Basket'
+
+
+class BasketItem(models.Model):
+    basket = models.ForeignKey(Basket, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.product.name} - {self.quantity}'
---
+++ orders/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.conf import settings
+from products.models import Product
+
+class Order(models.Model):
+    STATUS_CHOICES = (
+        ('Pending', 'Pending'),
+        ('Shipped', 'Shipped'),
+        ('Delivered', 'Delivered'),
+        ('Canceled', 'Canceled'),
+    )
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    order_items = models.ManyToManyField(Product, through='OrderItem')
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=255, choices=STATUS_CHOICES, default='Pending')
+
+    def __str__(self):
+        return f'{self.user.username} - Order {self.id}'
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.product.name} - {self.quantity}'
+
+class ShippingAddress(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, related_name='shipping_address')
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    address_line1 = models.CharField(max_length=255)
+    address_line2 = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=10)
+    country = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} - {self.address_line1}, {self.city}'
