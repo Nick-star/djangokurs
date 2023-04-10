fad17909ca577340857ef167fca06a52b16d1059
Nick-star <stavnikita@gmail.com>
2023-04-10 15:36:30 +0700

    Made index.html

--- products/models.py
+++ products/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    image = models.ImageField(upload_to='products/images', blank=True, null=True)
     stock = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     created_at = models.DateTimeField(auto_now_add=True)
