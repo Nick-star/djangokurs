8dc5e7c2482c411fa26f825952b00c9941e88634
Nick-star <stavnikita@gmail.com>
2023-04-12 18:39:33 +0700

    update_product.html

--- products/urls.py
+++ products/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
  path('', views.index, name='index'),
  path('add-product/', views.add_product, name='add_product'),
  path('product/<int:pk>/', views.product_detail, name='product_detail'),
+ path('product/<int:pk>/update/', views.update_product, name='update_product'),
+ path('product/<int:pk>/delete/', views.delete_product, name='delete_product'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic.edit import UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
 from .models import Product
 from .forms import ProductForm
 
 
+@login_required
 def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
@@ -29,3 +33,22 @@ def product_detail(request, pk):
         'product': product,
     }
     return render(request, 'product_detail.html', context)
+
+
+@login_required
+def update_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    form = ProductForm(request.POST or None, request.FILES or None, instance=product)
+    if form.is_valid():
+        form.save()
+        return redirect('products:product_detail', pk=pk)
+    return render(request, 'update_product.html', {'form': form})
+
+
+@login_required
+def delete_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('products:index')
+    return render(request, 'delete_product.html', {'product': product})
