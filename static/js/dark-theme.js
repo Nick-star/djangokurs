10d9fc6f795a50d1ac6e269d06759d6db612eb90
Ness <{yzenfer@mail.ru}>
2023-04-19 12:42:37 +0700

    svg img

--- static/js/dark-theme.js
+++ static/js/dark-theme.js
@@ -1,8 +1,8 @@
 document.getElementById("dark-theme").addEventListener("click", function() {
   document.body.classList.toggle("dark-theme");
   if (document.body.classList.contains("dark-theme")) {
-    document.getElementById("theme-icon").src = "{% static 'icons/sun.jpg' %}";
+    document.getElementById("theme-icon").src = "{% static 'icons/sun.svg' %}";
   } else {
-    document.getElementById("theme-icon").src = "{% static 'icons/dark.jpg' %}";
+    document.getElementById("theme-icon").src = "{% static 'icons/dark.svg' %}";
   }
 });
