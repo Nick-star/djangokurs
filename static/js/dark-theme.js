fb395a7ea76d43d04e53db98feeab6a8af39e2cb
Ness <{yzenfer@mail.ru}>
2023-04-19 12:29:20 +0700

    Dark theme css

--- static/js/dark-theme.js
+++ static/js/dark-theme.js
@@ -1,3 +1,8 @@
 document.getElementById("dark-theme").addEventListener("click", function() {
-    document.body.classList.toggle("dark-theme");
-    });
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    document.getElementById("theme-icon").src = "{% static 'icons/sun.jpg' %}";
+  } else {
+    document.getElementById("theme-icon").src = "{% static 'icons/dark.jpg' %}";
+  }
+});
